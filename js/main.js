e64241176748362570a19e730e7db919c8cf06c7
Nat Garrido <natgramirez@gmail.com>
2017-05-04 12:42:50 -0300

    dibujar en el html el contenido de los equipos y enlace con el boton

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function Pokemon(nombre, color, poderDeAtaque) {
+    this.lista = [];
     this.nombre = nombre;
     this.color = color;
 
@@ -10,6 +11,10 @@ function Pokemon(nombre, color, poderDeAtaque) {
         return ('Hola, soy: ' + this.nombre + 'y soy de color: ' + this.color);
     };
 
+    this.push = function(pokemon) {
+        this.lista.push(pokemon);
+    };
+
     this.aumentarAmistad = function(valor) {
         this.nivelDeAmistad = this.nivelDeAmistad + valor;
     };
@@ -19,17 +24,56 @@ function Pokemon(nombre, color, poderDeAtaque) {
     };
 }
 
+//Ingreso en un arreglo el equipo de Pokemon
+var lista = [];
 const Pikachu = new Pokemon('Pikachu', 'amarillo', 100);
 const Charmander = new Pokemon('Charmander', 'rojo', 20);
 const Vulpix = new Pokemon('Vulpix', 'naranjo', 50);
 const Lapras = new Pokemon('Lapras', 'celeste', 250);
+lista.push(Pikachu);
+lista.push(Charmander);
+lista.push(Vulpix);
+lista.push(Lapras);
 
-Pikachu.atacar(Charmander);
-//document.write('La vida de Charmander es: ', Charmander.vida);
-console.log(Charmander.vida);
+console.log(lista);
+
+//Dibujo el select de quien ataca
+function dibujarAtacan() {
+    var listaPokemon = document.getElementById('pokemon-ataca');
+    listaPokemon.innerHTML = '';
+    var nuevoHTML = '';
+    lista.forEach(function(pokemon) {
+        nuevoHTML += '<option value="' + pokemon.nombre + '">' + pokemon.nombre + '</option>';
+    });
+
+    listaPokemon.innerHTML = nuevoHTML;
+}
+
+//Dibujo el select del atacado
+function dibujarAtacados() {
+    var listaPokemonAtacado = document.getElementById('pokemon-atacado');
+    listaPokemonAtacado.innerHTML = '';
+    var nuevoHTML = '';
+    lista.forEach(function(pokemon) {
+        nuevoHTML += '<option value="' + pokemon.nombre + '">' + pokemon.nombre + '</option>';
+    });
+
+    listaPokemonAtacado.innerHTML = nuevoHTML;
+}
+
+//Llamo a las funciones que dibujan
+dibujarAtacan();
+dibujarAtacados();
+
 
 function pelear() {
-    var id1 = document.getElementById('pokemon-ataca');
-    console.log(id1);
+    //Obtiene el pokemon que va a atacar
+    var pokemonQueAtaca = document.getElementById("pokemon-ataca").value;
+    document.getElementById('ataca').innerHTML = 'El pokemon que ataca es ' + pokemonQueAtaca;
+
+    //Obtiene el pokemon atacado
+    var pokemonAtacado = document.getElementById('pokemon-atacado').value;
+    document.getElementById('atacado').innerHTML = 'El pokemon atacado es ' + pokemonAtacado;
+
 
 }
